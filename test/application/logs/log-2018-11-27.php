3c6dacc1aa9e0532b60e8c5127a2d11bf8110dc6
EC2 Default User <ec2-user@ip-172-31-22-12.ap-northeast-1.compute.internal>
2018-11-27 17:23:56 +0900

    Change webpack.config.js.dist

---
+++ test/application/logs/log-2018-11-27.php
@@ -0,0 +1,24 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2018-11-27 08:19:15 --> Config Class Initialized
+INFO - 2018-11-27 08:19:15 --> Hooks Class Initialized
+DEBUG - 2018-11-27 08:19:15 --> UTF-8 Support Enabled
+INFO - 2018-11-27 08:19:15 --> Utf8 Class Initialized
+INFO - 2018-11-27 08:19:15 --> URI Class Initialized
+DEBUG - 2018-11-27 08:19:15 --> No URI present. Default controller set.
+INFO - 2018-11-27 08:19:15 --> Router Class Initialized
+INFO - 2018-11-27 08:19:15 --> Output Class Initialized
+INFO - 2018-11-27 08:19:15 --> Security Class Initialized
+DEBUG - 2018-11-27 08:19:15 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-11-27 08:19:15 --> Input Class Initialized
+INFO - 2018-11-27 08:19:15 --> Language Class Initialized
+INFO - 2018-11-27 08:19:15 --> Loader Class Initialized
+INFO - 2018-11-27 08:19:15 --> Helper loaded: url_helper
+INFO - 2018-11-27 08:19:15 --> Session: Class initialized using 'files' driver.
+INFO - 2018-11-27 08:19:15 --> Controller Class Initialized
+INFO - 2018-11-27 08:19:15 --> AppModel class loaded
+INFO - 2018-11-27 08:19:15 --> Model "TestModel" initialized
+INFO - 2018-11-27 08:19:15 --> Database Driver Class Initialized
+ERROR - 2018-11-27 08:19:15 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'test' /var/www/html/codeigniter-extension/test/vendor/codeigniter/framework/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2018-11-27 08:19:15 --> Unable to connect to the database
+INFO - 2018-11-27 08:19:15 --> Language file loaded: language/english/db_lang.php
