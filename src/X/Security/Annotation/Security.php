668fdf6c9e8438f48f566b66f48b5b6ff6b37a5c
EC2 Default User <ec2-user@ip-172-31-22-12.ap-northeast-1.compute.internal>
2018-11-29 14:36:17 +0900

    Changed

--- src/X/Security/Annotation/Security.php
+++ src/X/Security/Annotation/Security.php
@@ -7,9 +7,6 @@ use Doctrine\Common\Annotations\Annotation;
  * Security annotation
  *
  * @Annotation
- * @author     Takuya Motoshima <https://www.facebook.com/takuya.motoshima.7>
- * @license    MIT License
- * @copyright  2018 Takuya Motoshima
  */
 class Security
 {
--- test/application/logs/log-2018-11-29.php
+++ test/application/logs/log-2018-11-29.php
@@ -42,3 +42,39 @@ INFO - 2018-11-29 05:33:43 --> Controller Class Initialized
 INFO - 2018-11-29 05:33:43 --> AppModel class loaded
 INFO - 2018-11-29 05:33:43 --> Model "TestModel" initialized
 ERROR - 2018-11-29 05:33:43 --> Severity: error --> Exception: [Semantical Error] The annotation "@Security" in method Test::index() was never imported. Did you maybe forget to add a "use" statement for this annotation? /var/www/html/codeigniter-extension/test/vendor/doctrine/annotations/lib/Doctrine/Common/Annotations/AnnotationException.php 54
+INFO - 2018-11-29 05:35:26 --> Config Class Initialized
+INFO - 2018-11-29 05:35:26 --> Hooks Class Initialized
+DEBUG - 2018-11-29 05:35:26 --> UTF-8 Support Enabled
+INFO - 2018-11-29 05:35:26 --> Utf8 Class Initialized
+INFO - 2018-11-29 05:35:26 --> URI Class Initialized
+INFO - 2018-11-29 05:35:26 --> Router Class Initialized
+INFO - 2018-11-29 05:35:26 --> Output Class Initialized
+INFO - 2018-11-29 05:35:26 --> Security Class Initialized
+DEBUG - 2018-11-29 05:35:26 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-11-29 05:35:26 --> Input Class Initialized
+INFO - 2018-11-29 05:35:26 --> Language Class Initialized
+INFO - 2018-11-29 05:35:26 --> Loader Class Initialized
+INFO - 2018-11-29 05:35:26 --> Helper loaded: url_helper
+INFO - 2018-11-29 05:35:26 --> Session: Class initialized using 'files' driver.
+INFO - 2018-11-29 05:35:26 --> Controller Class Initialized
+INFO - 2018-11-29 05:35:26 --> AppModel class loaded
+INFO - 2018-11-29 05:35:26 --> Model "TestModel" initialized
+ERROR - 2018-11-29 05:35:26 --> Severity: error --> Exception: [Semantical Error] The annotation "@Security" in method Test::index() was never imported. Did you maybe forget to add a "use" statement for this annotation? /var/www/html/codeigniter-extension/test/vendor/doctrine/annotations/lib/Doctrine/Common/Annotations/AnnotationException.php 54
+INFO - 2018-11-29 05:35:27 --> Config Class Initialized
+INFO - 2018-11-29 05:35:27 --> Hooks Class Initialized
+DEBUG - 2018-11-29 05:35:27 --> UTF-8 Support Enabled
+INFO - 2018-11-29 05:35:27 --> Utf8 Class Initialized
+INFO - 2018-11-29 05:35:27 --> URI Class Initialized
+INFO - 2018-11-29 05:35:27 --> Router Class Initialized
+INFO - 2018-11-29 05:35:27 --> Output Class Initialized
+INFO - 2018-11-29 05:35:27 --> Security Class Initialized
+DEBUG - 2018-11-29 05:35:27 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-11-29 05:35:27 --> Input Class Initialized
+INFO - 2018-11-29 05:35:27 --> Language Class Initialized
+INFO - 2018-11-29 05:35:27 --> Loader Class Initialized
+INFO - 2018-11-29 05:35:27 --> Helper loaded: url_helper
+INFO - 2018-11-29 05:35:27 --> Session: Class initialized using 'files' driver.
+INFO - 2018-11-29 05:35:27 --> Controller Class Initialized
+INFO - 2018-11-29 05:35:27 --> AppModel class loaded
+INFO - 2018-11-29 05:35:27 --> Model "TestModel" initialized
+ERROR - 2018-11-29 05:35:27 --> Severity: error --> Exception: [Semantical Error] The annotation "@Security" in method Test::index() was never imported. Did you maybe forget to add a "use" statement for this annotation? /var/www/html/codeigniter-extension/test/vendor/doctrine/annotations/lib/Doctrine/Common/Annotations/AnnotationException.php 54
