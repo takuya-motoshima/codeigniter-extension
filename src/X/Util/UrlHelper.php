185c2de9543831e225a9e1f4fba939af8a858d62
EC2 Default User <ec2-user@ip-172-31-22-12.ap-northeast-1.compute.internal>
2019-02-21 14:35:24 +0900

    Add UrlHelper

---
+++ src/X/Util/UrlHelper.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * URL helper class
+ *
+ * @author     Takuya Motoshima <https://www.facebook.com/takuya.motoshima.7>
+ * @license    MIT License
+ * @copyright  2017 Takuya Motoshima
+ */
+namespace X\Util;
+final class UrlHelper
+{
+
+  /**
+   * URL Without FileName
+   * 
+   * e.g:
+   *   https://example.com                        -> https://example.com
+   *   https://example.com?key=123                -> https://example.com
+   *   https://example.com/abc.html               -> https://example.com
+   *   https://example.com/abc.html?key=123       -> https://example.com
+   *   https://example.com/abc/xyz.html           -> https://example.com/abc
+   *   https://example.com/abc/xyz.html?key=123   -> http://example.com/abc
+   *   //example.com                             -> //example.com
+   *   //example.com/                            -> //example.com
+   *   //example.com/abc.html                    -> //example.com
+   *   //example.com/abc/xyz.html                -> //example.com/abc
+   *   //example.com?key=123                     -> //example.com
+   *   //example.com/?key=123                    -> //example.com
+   *   //example.com/abc.html?key=123            -> //example.com
+   *   //example.com/abc/xyz.html?key=123        -> //example.com/abc
+   *   
+   * @param  string $url
+   * @return string
+   */
+  function getUrlWithoutFileName(string $url): string
+  {
+    $url = rtrim(strtok($url, '?'), '/');
+    $urlInfo = parse_url($url);
+    $scheme = !empty($urlInfo['scheme']) ? $urlInfo['scheme'] . '://' : '//';
+    $host = $urlInfo['host'];
+    $path = $urlInfo['path'] ?? '';
+    if (!empty($path)) {
+      $pathInfo = pathinfo($path);
+      if (isset($pathInfo['extension'])) {
+        $path = rtrim(str_replace($pathInfo['filename'] . '.' . $pathInfo['extension'], '', $path), '/');
+      }
+    }
+    return $scheme . $host . $path;
+  }
+}
